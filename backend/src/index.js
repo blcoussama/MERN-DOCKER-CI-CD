9f08c8fbf7f09e3994ebdf88cb0b2dfc57dbfa56
blcoussama <belcadioussama.eng.com>
2025-02-20 21:30:27 +0100

    prod fixes 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,11 +51,11 @@ app.use("/api/message", MessageRoutes)
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    }); 
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+    });
 }
 
 // Start the server only after connecting to the database
